901428c616b0fff3a7e835497a6e266da7d52ae1
xD'Criz <32378563+criz16@users.noreply.github.com>
2022-01-09 01:13:10 +0800

    Update adopisoft-4.1.2.sh

--- adopisoft-4.1.2.sh
+++ adopisoft-4.1.2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ###############################################################################################################
- 
+ln -fs /usr/share/zoneinfo/Asia/Manila /etc/localtime
 sudo apt-get update && sudo apt-get full-upgrade -y
 sudo apt-get install curl -y
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
@@ -16,12 +16,12 @@ sudo apt-get install ./adopisoft-4.1.2-release-amd64-node-v12.18.3.deb
 #sudo systemctl disable ssh
  
  
-#wget https://gist.githubusercontent.com/alenteria/791dbe32175a01d1f1b602b25489ad22/raw/9a5aa879ac70d24bd9a7dd7f8ed97d7fe2c2f597/ado-psql-script.sh 
+wget https://gist.githubusercontent.com/alenteria/791dbe32175a01d1f1b602b25489ad22/raw/9a5aa879ac70d24bd9a7dd7f8ed97d7fe2c2f597/ado-psql-script.sh 
  
-#sudo chmod a+x ./ado-psql-script.sh
-#sudo ./ado-psql-script.sh
+sudo chmod a+x ./ado-psql-script.sh
+sudo ./ado-psql-script.sh
 --- if the above command failed, try the following: ---
-#bash ./ado-psql-script.sh
+bash ./ado-psql-script.sh
  
  
 ip addr
